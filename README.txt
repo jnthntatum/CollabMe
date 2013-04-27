78df7be5f2df6403cc8dc666940a73b7bb71e2ec
CS194 Project <cs194@ancho.(none)>
2013-04-26 18:38:09 -0700

    server up and running

---
+++ README.txt
@@ -0,0 +1,4 @@
+Collab Me Home Directory (git repo root)
+Add information where relevant
+
+~jon
---
+++ rapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rapp::Application.config.secret_token = 'e4e60e11ce5e96ed04ef2a11d4a770e92a32718e8faa6bee9d2f28736ca0684e98abdd372bfbfef77b91fd1cb45dcc3a4e4d5d7aa85de08b4e1be591765e6a61'
