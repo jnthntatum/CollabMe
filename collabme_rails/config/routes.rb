f38f1f7b2f2364bb537630f74fb321efcbbd7da5
jdtatum <jdtatum@stanford.edu>
2013-04-29 13:14:31 -0700

    updated with basic formatting

--- collabme_rails/config/routes.rb
+++ collabme_rails/config/routes.rb
@@ -1,6 +1,10 @@
 CollabmeRails::Application.routes.draw do
   #get "home/index"
 
+# You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,9 +52,7 @@ CollabmeRails::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
