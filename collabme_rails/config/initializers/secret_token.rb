2897778a73321c3c9344df75a9c1adc56f8d4047
CS194 Project <cs194@ancho.(none)>
2013-04-28 22:11:44 -0700

    created rails installer install

---
+++ collabme_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollabmeRails::Application.config.secret_token = '221703fdd9182f17ad9fee4c21b61b18b9f8f3c23a715c3cec888581a87ae9dc9f03af9dc9e45739f4f6e469c91eb26e48856d81b850cfc1b4ce7c408c84879a'
