5d05111623c8df2384e0e8cc58c8a0f98397c30b
Jonathan TatumOB <jdtatum@stanford.edu>
2013-05-20 14:17:08 -0700

    chatserver edits

--- whiteboardserver/wbprimatives.js
+++ whiteboardserver/wbprimatives.js
@@ -24,17 +24,16 @@ function Squiggle(x,y){
 }
 Squiggle.prototype  = new Drawable()
 
-Squiggle.prototype.draw = function( Canvas ){
-	Canvas.beginPath();
-	Canvas.lineWidth=this.lineWidth;
-	Canvas.strokeStyle=this.color; 
-	Canvas.moveTo(this.x, this.y);
+Squiggle.prototype.draw = function(	canvas ){
+	canvas.beginPath();
+	canvas.lineWidth=this.lineWidth;
+	canvas.strokeStyle=this.color; 
+	canvas.moveTo(this.x, this.y);
 	for (var i = 0; i < this.pts.length; i ++){
 		pt = this.pts[i];
-		Canvas.lineTo(pt[0], pt[1]);
+		canvas.lineTo(pt[0], pt[1]);
 	}
-	//Canvas.closePath()
-	Canvas.stroke();
+	canvas.stroke();
 }
 
 
@@ -48,11 +47,21 @@ TextBox.prototype = new Drawable();
 
 TextBox.prototype.draw = function ( canvas ){
 // TODO:
+	
+	context.font = '10px sans-serif'
+	context.fillStyle = 'black';
+	context.fillText(this.text, this.x, this.y); 
 }
 
+function ChatMessage (name, text){
+	this.name = name;
+	this.text = text;
+	this.type = 'chat_message';
+}
 
 if (typeof module == 'object' && module && typeof module.exports == 'object' && module.exports){
 	module.exports.Squiggle = Squiggle;
 	module.exports.TextBox = TextBox;
 	module.exports.Drawable = Drawable;
+	module.exports.ChatMessage = ChatMessage; 
 }
--- whiteboardserver/wbserver.js
+++ whiteboardserver/wbserver.js
@@ -22,12 +22,109 @@ server.listen(8989);
 // Create a Socket.IO instance, passing it our server
 var io = si.listen(server);
 
+function Session(){
+	this.users = []
+	this.drawables = []
+	this.messages = []
+}
+
+var sessions = []; 
+
+function UserMap(){
+	this.userToClient = {}
+	this.clientToUser = {}
+	this.getUser = function(client){
+		return this.clientToUser[client]  
+	}
+	this.getClient= function(uid){
+		return this.userToClient[uid]
+	}
+	this.put = function(uid, client){
+		if (uid in this.userToClient){
+			return false;
+		} else {
+			this.userToClient[uid] = client;
+			this.clientToUser[client] = uid;
+		}
+	}
+	this.remove = function (uid){
+
+		if (uid in this.userToClient){
+			var cli = this.userToClient[uid];
+			delete this.userToClient[uid] 
+			delete this.clientToUser[cli]
+			return true;
+		}else{
+			return false; 
+		}
+	}
+	this.userIn(uid){
+		return uid in this.userToClient; 
+	}
+}
+
+var users = new UserMap();  
+
+function Message(command, uid){
+	this.message = command;
+	this.uid = uid; 
+}
+
+function sendError(client, message, error){
+	var m = new Message("ERROR", 0) 
+	m.cause = message; 
+	m.details =  error;  
+	sendMessage(client, m) 
+}
+
+function sendMessage(client, message){
+	console.log("send message to" + client.id + ": " + message.command)
+}
+
+function validateMessage(message){
+	if (typeof message.uid === 'number' && message.uid > 0 &&
+	 	typeof message.command === 'string'){
+		return true;
+	}else 
+		return false; 
+}
+
+function procMessage(client, message){
+	if (!validateMessage(message)){
+		sendError(client, message, "malformed message header"); 
+	}
+	var command = message.command;
+	var uid = message.uid; 
+	if (command === "STATUS"){
+		sessions.push(uid);  
+		users.put(uid, client.id);
+		if (typeof message.uids ==='object' && message.uids instanceof Array)
+			for (var i = 0; i < message.uids.length; i++){
+				if (users.hasUser(uid))
+					sessions.push(uid)
+			} 
+		var m = new Message("ACK", );
+	} else if(command === "CREATE"){
+
+	} else if(command === "LEAVE"){
+		
+	} else if(command === "ADD"){
+		
+	} else if(command === "POST"){
+		
+	} else if(command === "CREATE"){
+		
+	} else {
+		sendError(client, message, "unrecognized command")
+	}
+}
+
 // Add a connect listener
 io.sockets.on('connection', function(client){ 
 	
 	// Success!  Now listen to messages to be received
 	client.on('message',function(event){ 
-		console.log('Received message from client!',event);
+		procMessage(client, event)
 	});
 
 	client.on('disconnect',function(){
