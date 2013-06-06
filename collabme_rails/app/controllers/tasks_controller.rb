17c8be6ec7c44fbb05088f07c930669635e0e926
Syed Shayon Saleh <ssaleh@stanford.edu>
2013-06-05 22:47:23 -0700

    Can add a new task when logged in. Now need to allow editing of task

--- collabme_rails/app/controllers/tasks_controller.rb
+++ collabme_rails/app/controllers/tasks_controller.rb
@@ -1,6 +1,15 @@
 class TasksController < ApplicationController
 
 def create
+  @task = Task.new()
+  @task.desc = params[:task][:desc]
+  @task.project = Project.find_by_id(params[:task][:project_id])
+  @task.creator = Researcher.find_by_id(params[:task][:creator_id])
+  @task.assignee = Researcher.find_by_id(params[:task][:assignee_id])
+  @task.category = params[:task][:category]
+  if @task.save()
+    redirect_to project_path(@task.project)
+  end
 end
 
 end
--- collabme_rails/app/models/task.rb
+++ collabme_rails/app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
-  attr_accessible :desc, :category
+  attr_accessible :desc, :category, :assignee
   belongs_to :project
   belongs_to :creator, :class_name => "Researcher", :foreign_key => "creator_id"
   belongs_to :assignee, :class_name => "Researcher", :foreign_key => "assignee_id"
